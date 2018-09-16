b19f970ad3a8fffdbfcaa603c112c5a63124373e
Ryan H <ryan@murage.ca>
2018-09-16 08:16:41 -0700

    Add sqlite3 interface

---
+++ src/sqlite3_pinyincompare.c
@@ -0,0 +1,24 @@
+#include <sqlite3ext.h>
+SQLITE_EXTENSION_INIT1
+
+#include "pinyin_compare.h"
+
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+/* TODO: Change the entry point name so that "extension" is replaced by
+** text derived from the shared library filename as follows:  Copy every
+** ASCII alphabetic character from the filename after the last "/" through
+** the next following ".", converting each character to lowercase, and
+** discarding the first three characters if they are "lib".
+*/
+int sqlite3_pinyincompare_init(
+  sqlite3 *db, 
+  char **pzErrMsg, 
+  const sqlite3_api_routines *pApi
+){
+  int rc = SQLITE_OK;
+  SQLITE_EXTENSION_INIT2(pApi);
+  sqlite3_create_collation(db, "pinyin", SQLITE_UTF8, NULL, pinyin_compare);
+  return rc;
+}
